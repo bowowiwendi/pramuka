bafb9dea9b545871d4411b0cd8c0aab0bdcb1214
bowowiwendi <bowowiwendi@gmail.com>
2026-05-22 19:46:49 +0000

    feat: jadikan aplikasi PWA (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'game-pramuka-v1';
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.png',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.pathname.startsWith('/api/')) {
+    e.respondWith(fetch(request).catch(() => new Response(JSON.stringify([]), { headers: { 'Content-Type': 'application/json' } })));
+    return;
+  }
+  e.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((res) => {
+      const clone = res.clone();
+      caches.open(CACHE).then((c) => c.put(request, clone));
+      return res;
+    }))
+  );
+});
